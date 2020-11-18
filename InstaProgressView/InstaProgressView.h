3e6c546c92af98f78a253c5dc47bf2c7c69f86a3
Ed <esinyakov@baltbet.ru>
2020-11-19 01:16:30 +0300

    Initial Commit

---
+++ InstaProgressView/InstaProgressView.h
@@ -0,0 +1,18 @@
+//
+//  InstaProgressView.h
+//  InstaProgressView
+//
+//  Created by Eduard Sinyakov on 11/19/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InstaProgressView.
+FOUNDATION_EXPORT double InstaProgressViewVersionNumber;
+
+//! Project version string for InstaProgressView.
+FOUNDATION_EXPORT const unsigned char InstaProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstaProgressView/PublicHeader.h>
+
+
